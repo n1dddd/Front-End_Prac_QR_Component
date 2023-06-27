f6e24238311fad36cb11d438515554a1ad52c0e2
Daniel <danielseniv@outlook.com>
2023-06-26 21:39:54 -0400

    Change id selector to className selector in element(s)

--- QR-Code-Component/src/components/QR_code.jsx
+++ QR-Code-Component/src/components/QR_code.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function QR_code() {
   return (
-    <div className='QR_card'>
-      <img src="src/imgs/image-qr-code.png" id="QR_img" alt="QR_Code_Front-end_Mentor" />
-      <p id="QR_paragraph_1">Improve your front-end skills by building projects</p>
-      <p id="QR_paragraph_2">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    <div className='card'>
+      <img src="src/imgs/image-qr-code.png" className="card_img" alt="QR_Code_Front-end_Mentor" />
+      <p className="card_paragraph_1">Improve your front-end skills by building projects</p>
+      <p className="card_paragraph_2">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
 
   )
