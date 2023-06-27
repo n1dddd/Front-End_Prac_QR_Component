afd17203e2dc5361a9af5058399331231be5772c
Daniel <danielseniv@outlook.com>
2023-06-26 21:16:57 -0400

    Create QR-Code component, copy styling and text from sample

--- QR-Code-Component/src/components/QR_code.jsx
+++ QR-Code-Component/src/components/QR_code.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function QR_code() {
   return (
-    <div>QR-code</div>
+    <div className='QR_card'>
+      <img src="src/imgs/image-qr-code.png" id="QR_img" alt="QR_Code_Front-end_Mentor" />
+      <p id="QR_paragraph_1">Improve your front-end skills by building projects</p>
+      <p id="QR_paragraph_2">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+
   )
 }
 
