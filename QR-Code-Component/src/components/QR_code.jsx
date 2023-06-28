55c533c27b586823c520ba1b81202fb6aa769c2d
Daniel <danielseniv@outlook.com>
2023-06-28 17:49:23 -0400

    Change how img file is imported

--- QR-Code-Component/src/components/QR_code.jsx
+++ QR-Code-Component/src/components/QR_code.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import qrCode from '/public/image-qr-code.png';
 
 function QR_code() {
   return (
     <div className='card'>
-      <img src="/public/image-qr-code.png" className="card_img" alt="QR_Code_Front-end_Mentor" />
+      <img src={qrCode} className="card_img" alt="QR_Code_Front-end_Mentor" />
       <p className="card_paragraph_1">Improve your front-end skills by building projects</p>
       <p className="card_paragraph_2">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
