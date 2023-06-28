6ca6102ce016cdd47be18431a380090c864f9fb6
Daniel <danielseniv@outlook.com>
2023-06-28 17:38:31 -0400

    Change file path for img

--- QR-Code-Component/src/components/QR_code.jsx
+++ QR-Code-Component/src/components/QR_code.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function QR_code() {
   return (
     <div className='card'>
-      <img src="src/imgs/image-qr-code.png" className="card_img" alt="QR_Code_Front-end_Mentor" />
+      <img src="/src/imgs/image-qr-code.png" className="card_img" alt="QR_Code_Front-end_Mentor" />
       <p className="card_paragraph_1">Improve your front-end skills by building projects</p>
       <p className="card_paragraph_2">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
